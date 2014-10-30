66375d5444423a33b9fdf9502e1293bd149c1bfc
nd706 <nizar_sd@yahoo.com>
2014-10-30 12:33:56 +0000

    dclk tx and rx

--- par_clib/dclk_rx.v
+++ par_clib/dclk_rx.v
@@ -14,39 +14,46 @@ module dclk_rx (rclk, wclk, reset, valid, channel_busy, item_read, serial_in, pa
 	
 	reg [`PAYLOAD_SIZE+`ADDR_BITS:0] item;
 	
+	reg [1:0] valid_reg;
+	
+	wire validw;
+	
 	assign parallel_out = item;
 	
-	assign valid = (state == 2);
+	assign validw = (state == 2);
+	
+	assign valid =  valid_reg[1];  // synchronised to rclk
+	
 	
 	assign channel_busy = (state != 0);
 	
 	always @(posedge rclk) begin
 	
+		valid_reg[0] <= validw;  // synchronise "valid" to rclk
+		valid_reg[1] <= valid_reg[0];
+		
+	
 		if (reset) begin
 
-			item <= 0;
-			
-			state <= 0;
+		    item <= 0;
+		    
+		    state <= 0;
+		    
+		    valid_reg <= 0 ;
 			
 		end else begin
 		
-			if (state == 0 & serial_in) begin
-			
-				// transition from 'idle' to 'receiving' on arrival of flit head (high bit)
-			
-				state <= 1;				
-			
-			end 
-			
-			if (state == 2 & item_read) begin
-			
-				// item transferred, switch back to 'idle'
-			
-				state <= 0;
-				
-				item <= 0;
-				
-			end
+		    if (state == 2 & item_read) begin
+		    
+			    // item transferred, switch back to 'idle'
+		    
+			    state <= 0;
+			    
+			    item <= 0;
+			    
+			    valid_reg <= 0 ;
+			    
+		    end
 		
 		end
 	
@@ -62,6 +69,15 @@ module dclk_rx (rclk, wclk, reset, valid, channel_busy, item_read, serial_in, pa
 			state <= 0;
 			
 		end else begin
+			
+			if (state == 0 & serial_in) begin
+			
+				// transition from 'idle' to 'receiving' on arrival of flit head (high bit)
+			
+				state <= 1;				
+			
+			end 
+			
 		
 			if (state != 2) begin
 			
@@ -73,7 +89,7 @@ module dclk_rx (rclk, wclk, reset, valid, channel_busy, item_read, serial_in, pa
 				
 				if (item[0]) begin
 					state <= 2; // item received when LSB is 1
-					//if (routerid > -1) $display("router %d %s rx : %d", routerid, port, item>>1);
+					//if (routerid > -1) $display("router %d %s rx : %d", routerid, port, item >> 1);
 				end
 			
 			end 
---
+++ par_clib/dclk_tx.v
@@ -0,0 +1,80 @@
+
+module dclk_tx (clk, reset, req, tx_busy, channel_busy, parallel_in, serial_out, tx_active);
+
+	parameter routerid=-1;
+	parameter port="unknown";
+
+	input clk, reset, req, channel_busy;
+	
+	output tx_busy, serial_out;
+	
+	input [`PAYLOAD_SIZE+`ADDR_BITS-1:0] parallel_in;
+	
+	output reg tx_active;
+	
+	reg [`PAYLOAD_SIZE+`ADDR_BITS+1:0] item;
+	
+	reg [`PAYLOAD_SIZE+`ADDR_BITS-1:0] temp;
+	
+	reg [1:0] busy_reg;
+	
+	wire busy_sync;
+	
+	assign busy_sync = busy_reg[1];
+	
+	
+	assign serial_out = item[0] & tx_active;
+	
+	assign tx_busy = tx_active | busy_sync;
+	
+	always @(posedge clk) begin
+	
+		if (reset) begin
+		
+			item <= 0;
+			tx_active <= 0;
+			temp <= 0;
+			busy_reg<=0;
+		
+		end else begin
+			busy_reg[0] <= channel_busy;  // synchronise "channel_busy" to clk
+			busy_reg[1] <= busy_reg[0];
+			
+			if (!tx_active) begin
+			
+				if (!busy_sync & req) begin
+				
+					// switch to 'transmitting' state
+			
+					tx_active <= 1;
+					
+					item[`PAYLOAD_SIZE+`ADDR_BITS:1] <= parallel_in;
+					temp <= parallel_in;
+					item[`PAYLOAD_SIZE+`ADDR_BITS+1] <= 1;
+					item[0] <= 1;
+					
+					//if (!tx_active && (routerid > -1)) $display("router %d %s tx : %d", routerid, port, parallel_in);
+					
+					//item <= {1'b1, parallel_in[`ADDR_BITS-1:0], 1'b1}; // leading one is the start bit of the flit
+					
+				end
+			
+			end else begin
+
+				item <= (item >> 1);
+				
+				if ((item>>1) == 1) begin  // the last (stop bit) ?
+					
+					// end transmission when the currently transmitted bit is the last
+					
+					tx_active <= 0;
+					item <= 0;
+				end
+			
+			end
+		
+		end
+	
+	end
+
+endmodule
