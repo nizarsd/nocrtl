81c75b12276336a9d207c4f154591a0261088668
nd706 <nizar_sd@yahoo.com>
2014-11-06 12:25:48 +0000

    async testbench and NI_v1

--- par_clib/NI_v1.v
+++ par_clib/NI_v1.v
@@ -1,197 +1,137 @@
-
-module par_source_from_memory (clk, reset, item_out, valid, busy, send);
-	parameter id = -1;
+module NI(clk, reset, item_out, req, channel_busy, send, item_in, valid, busy, error, led);
+    
+    parameter id = -1;
+    
+    input clk, reset, channel_busy, valid, send;
+    
+    output req, busy, error, led;
+    
+    output  [`HDR_SZ + `PL_SZ + `ADDR_SZ-1:0] item_out;
+    
+    input   [`HDR_SZ + `PL_SZ + `ADDR_SZ-1:0] item_in;
+    
+    reg   [`HDR_SZ + `PL_SZ + `ADDR_SZ-1:0] item_out;
+    
+    
+//     reg   [`HDR_SZ + `PL_SZ + `ADDR_SZ-1:0] item_in;
+    
+    reg [`PL_SZ-1:0] counter;
+    
+    reg busy, error, led;
+    
+    reg req;
+    
+    reg  [`HDR_SZ-2 : 0] header; 
+    
+    wire [`ADDR_SZ-1:0] dest;
+    
+    wire [`PL_SZ-1:0] data_in;
+    
+    wire [`HDR_SZ + `PL_SZ + `ADDR_SZ - 2:0] packet;
+    
+    
+    
+    assign packet = {header, counter, dest};
+    
+    assign data_in = item_in[`PL_SZ + `ADDR_SZ-1:`ADDR_SZ];
+    
+//     assign  item_out[`HDR_SZ + `PL_SZ + `ADDR_SZ-1] = ^item_out [`HDR_SZ + `PL_SZ + `ADDR_SZ-2:0];
+    
+    
+    assign dest = (id == 0) ? 1 : 0;
+    
+    reg started, done; 
+    
+    wire [`ADDR_SZ-1:0] dest_ini;
+    
+//     assign dest_ini=(id==0)? 1 : 0;
+        
+    always @(posedge clk or posedge reset) begin
 	
-	parameter dests = 1;
-	
-	parameter pir=16;
-	
-	parameter traffic_file = "";
-	
-	input clk, reset, busy, send;
+	if (reset) begin
 		
-	output [`HDR_SZ + `PL_SZ + `ADDR_SZ-1:0] item_out;
-	
-	output valid;
-
-	wire [`HDR_SZ + `PL_SZ + `ADDR_SZ-1:0] data;
-	
-	source_from_memory #(id, dests, pir, traffic_file) s1 (clk, reset, data, req, busy, send);
-	
-	assign item_out = data;
-	
-	assign valid = req;
-	
-endmodule
-
-
-module moody_sink (clk, reset, data, req, busy);
-
-	parameter id = -1;
-
-	parameter hospitality = 0; // min 0, max 255
-
-	input clk, reset, req;
-	
-	output busy;
-	
-	input [`HDR_SZ + `PL_SZ + `ADDR_SZ-1:0] data;
-	
-	reg [`PL_SZ-1:0] register;
-
-	reg [`ADDR_SZ-1:0] dest_addr;
-	
-	reg busy;
-	
-// 	reg [7:0] rand;
-	
-	always @(posedge clk or posedge reset) begin
-	
-		if (reset) begin
-			register <= 0;
-			busy <= 0;
-// 			rand <= 0;
-		end else begin
-// 			rand <= $random;
-			if (req & !busy) begin
-				register <= data[`PL_SZ + `ADDR_SZ-1:`ADDR_SZ] ;
-				dest_addr <= data[`ADDR_SZ-1:0];
-				if (id != -1) $display ("##,rx,%d,%d",id, data[`PL_SZ + `ADDR_SZ-1:`ADDR_SZ]);
-// 				if (id != data[`ADDR_SZ-1:0]) $display ("*****rx violation in %d, %d -> %d @ %d",id,  data[`HDR_SZ + `PL_SZ + `ADDR_SZ-1:`PL_SZ + `ADDR_SZ],data[`ADDR_SZ-1:0],$time );
-// 				busy <= 1;
-			end else 
-			busy <= 0;//(rand > hospitality);
-		end			
-	
-	end
-
-endmodule
-
-
-
-module source_from_memory (clk, reset, data, req, busy, send);
-
-	parameter id = -1;
-	
-	parameter dests = 1;
-	
-	parameter pir=16;
-	
-	parameter traffic_file = "";
-	
-	parameter msg_size = 12																																																																																																																																																																																																																																																																																																																																																																																																													;
-	
-	input clk, reset, busy, send;
-	
-	output req;
-	
-	output [`HDR_SZ + `PL_SZ + `ADDR_SZ-1:0] data;
-	
-	reg [`HDR_SZ + `PL_SZ + `ADDR_SZ-1:0] data;
-	
-	reg counter;
-	
-	reg req;
-
-	reg pause;`HDR_SZ + 
-
-	reg done;
-	
-	reg fire; 
-	
-	reg [3:0] index;
-	
-	reg [3:0] dindex;
-	
-	reg [7:0] rand;
-	
-	reg [`ADDR_SZ-1:0] memory [0:`NUM_NODES-1];
-	reg [`HDR_SZ-1:0] dmemory [0:msg_size-1];
-	
-	initial $readmemh(traffic_file, memory, 0, dests-1) ;
-	initial $readmemh("data2.hex", dmemory, 0, msg_size-1) ;
-	
-	wire [`ADDR_SZ-1:0] dest;
-	
-	assign dest=memory[index];
-
-	wire can_send;
-	
-	assign can_send =!(pause | busy);
-	
-	always @(posedge clk or posedge reset) begin
-	
-		if (reset) begin
-			
-			data <= 0;
-			
-			fire<=1; 
-			
-			req <= 0;
-			
-			pause <= 0;
-			
-			counter <= 1;
-			
-			index <= 0;
-			
-			dindex <= 0;
-			
-			done <= 0;
-			
-		end 
-		else begin
-		      
-		rand<=$random;
+		  item_out <= 0;
+		  
+		  req <= 0;
+		  
+		  counter <= 0;
+		  
+		  busy <= 0;
+		  
+		  done <= 0;
+		  
+		  started <= 0 ;
+		  
+		  error <= 0 ;
+		  
+		  led <= 0 ;
+		  
+		  header <= 0;
 		
-	        if (!busy & req) $display ("##,tx,%d,%d",data[`ADDR_SZ-1:0],id);
- 	
-		if (pause) pause <=0;
-		      	      
-       		if (can_send & send & !done) begin
-// 		if (can_send & send) begin
+	end 
+	else begin
+	      
+	      // receive 
+	    if (valid & !busy & !done) begin
+		  
+// 		    if (counter[`PL_SZ-15] &&  id==0) begin
+		    if (counter[`PL_SZ-22] &&  id==0) begin
+
+		    
+			  $display ("counter = %d, time = %d, id=%d", counter, $time, id);
+
+			  counter <= 0;
 			  
-			  if (fire) begin			
-				    if ( id==0 & dest != id ) begin
-					  
-					  data[`ADDR_SZ-1:0] <= dest;
+			  led <= ! led;
+			  
+// 			  done <= 1;
 
-					  data[`PL_SZ + `ADDR_SZ-1:`ADDR_SZ] <= dmemory[dindex];
-				    
-					  req <= 1;
-					  
-					  pause <=1;
-					  
-					  if (id != -1) $display ("source %d -> %d: %c", id, dest, dmemory[dindex]);
-				    
-				    end
-											  
-				      counter <= counter + 1;
-					  
-				      index <= index + 1;
-				      
-				    
-				    if (index == dests-1) begin
-					
-					index <=0;
-					
-					dindex <= dindex + 1;
-					
-					if (dindex == msg_size-1) done <= 1;
-					
-				    end
-				  
-			  end
-				  
-		  end // busy
-		  else begin
+			  started <= 0;
 
-		      req <= 0;
-		  end
-		  fire <= 1; //(rand < pir);
-			
-		end //else reset
+		    end else begin
+		    
+			  counter <= item_in[`PL_SZ + `ADDR_SZ - 1 : `ADDR_SZ] + 1;
+			  
+// 			  if (started & id==0) $display ("##rx %d:  %d,%d @%d",id, counter, item_in[`PL_SZ + `ADDR_SZ-1:`ADDR_SZ], $time);
+
+			  if (started & id==0 & !(counter < data_in)) error <= 1;
+		    
+		    end
+		    
+		    busy <= 1;
+	      		    
+		    if (started == 0) started <= 1;
+      
+	      end 
+	      
+//	       process and send 
+
+// 	      if (!channel_busy & req) $display ("##,tx,%d,%d @ %d",item_out[`ADDR_SZ-1:0],id,$time );
+	      
+	      if (!channel_busy & send) begin
+	      
+		    if (busy | (id==0 & !started)) begin
+			    
+			  item_out[`HDR_SZ + `PL_SZ + `ADDR_SZ - 2 : 0] <= packet;
+  			  
+  			  item_out[`HDR_SZ + `PL_SZ + `ADDR_SZ-1] <= ^packet;  // parity bit
+
+			  req <= 1;
+			  
+			  busy <= 0;
+			  
+			  if (started == 0) started <= 1;
+		     end  
+		     else   req <= 0;
+		
+	      end /* channel_busy */ 
+	      else   req <= 0;
+		
+	end // !reset
 	
-	end // always
+    end // always
+
 
 endmodule
+
+   
---
+++ tb_2nodes.v
@@ -0,0 +1,98 @@
+`timescale 1ns/1ns
+
+`include "async_clib/constants.v"
+`include "par_clib/fifo.v"
+`include "par_clib/routing_logic_v2.v"
+// `include "par_clib/par_rx.v"
+// `include "par_clib/par_tx.v"
+// `include "par_clib/par_source_traffic.v"
+// `include "par_clib/par_source_data.v"
+`include "par_clib/par_rx_logic.v"
+// `include "par_clib/par_moody_sink.v"
+`include "par_clib/routing_table_logic.v"	
+`include "par_clib/rr_arbiter.v"
+`include "par_clib/ch_rx_logic.v"
+`include "par_clib/NI_v1.v"
+
+`include "async_clib/dclk_rx.v"
+`include "async_clib/dclk_tx.v"
+`include "async_clib/async_router.v"
+`include "async_clib/async_generator.v"
+
+	
+module testbench(); 
+
+// 	wire reset, clk;
+		
+	// routers:
+	// -----------------------------------------------------------------
+	
+	wire [`DIRECTIONS-1:0] rx_busy [`NUM_NODES-1:0];
+	wire [`DIRECTIONS-2:0] rx_data [`NUM_NODES-1:0];
+	wire [`DIRECTIONS-1:0] tx_busy [`NUM_NODES-1:0];
+	wire [`DIRECTIONS-2:0] tx_data [`NUM_NODES-1:0];
+	
+	wire  rx_l_valid [`NUM_NODES-1:0];
+	wire  tx_l_valid [`NUM_NODES-1:0];
+	wire [`HDR_SZ + `PL_SZ + `ADDR_SZ-1:0]  rx_l_data [`NUM_NODES-1:0];
+	wire [`HDR_SZ + `PL_SZ + `ADDR_SZ-1:0]  tx_l_data [`NUM_NODES-1:0];
+	
+	wire  router_active [`NUM_NODES-1:0];
+	wire [19:0] flit_counter [`NUM_NODES-1:0];
+		
+// 	clocks 
+	wire [`DIRECTIONS-2:0] wclk [`NUM_NODES-1:0];
+	wire [`NUM_NODES-1:0] clk ;
+	wire [`NUM_NODES-1:0] clk_fw ;	
+	
+	
+	wire [`HDR_SZ + `PL_SZ + `ADDR_SZ-1:0] source_data [`NUM_NODES-1:0];
+	wire source_busy [`NUM_NODES-1:0];
+	wire source_valid [`NUM_NODES-1:0];
+	
+	
+	wire [`HDR_SZ + `PL_SZ + `ADDR_SZ-1:0] sink_data [`NUM_NODES-1:0];
+	wire sink_busy [`NUM_NODES-1:0];
+	wire sink_valid [`NUM_NODES-1:0];
+	
+	async_gen g1 (clk, reset, send);
+
+
+	
+	async_router  #(0,"routing_tables/0.txt") 
+	  router0 (clk[0], wclk[0], clk_fw[0], reset, rx_busy[0], rx_data[0], rx_l_data[0], rx_l_valid[0], tx_busy[0], tx_data[0], tx_l_data[0], tx_l_valid[0], flit_counter[0]);	
+	
+	async_router  #(1,"routing_tables/1.txt") 
+	  router1 (clk[1], wclk[1], clk_fw[1], reset, rx_busy[1], rx_data[1], rx_l_data[1], rx_l_valid[1], tx_busy[1], tx_data[1], tx_l_data[1], tx_l_valid[1], flit_counter[1]);	
+	  
+	  
+	  NI #(0) NI0 (
+			.clk(clk[0]), 
+			.reset(reset), 
+			.item_out(source_data[0]), 
+			.req(source_valid[0]), 
+			.channel_busy(source_busy[0]), 
+			.send(send), 
+			.item_in(sink_data[0]), 
+			.valid(sink_valid[0]), 
+			.busy(sink_busy[0])
+		      );
+	  
+	  NI #(1) NI1 (
+			.clk(clk[1]), 
+			.reset(reset), 
+			.item_out(source_data[1]), 
+			.req(source_valid[1]), 
+			.channel_busy(source_busy[1]), 
+			.send(send), 
+			.item_in(sink_data[1]), 
+			.valid(sink_valid[1]), 
+			.busy(sink_busy[1])
+		      );
+	
+	`include "async_clib/create_noc/connections_2nodes.v"
+	    
+		      
+	  
+	
+endmodule
